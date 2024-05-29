64c833073bd2540be6829ad90b1ffccdc8477845
XiaofeiCao <xiaofeicao@microsoft.com>
2024-05-29 16:44:02 +0800

    add test

--- typespec-tests/src/test/java/com/arm/models/resources/ArmResourcesTests.java
+++ typespec-tests/src/test/java/com/arm/models/resources/ArmResourcesTests.java
@@ -1,22 +1,20 @@
 package com.arm.models.resources;
 
-import com.arm.models.resources.models.TopLevelArmResource;
-import com.arm.models.resources.models.TopLevelArmResourceProperties;
-import com.azure.core.credential.AccessToken;
-import com.azure.core.credential.TokenCredential;
-import com.azure.core.credential.TokenRequestContext;
+import com.azure.arm.models.resources.ResourcesManager;
+import com.azure.arm.models.resources.models.NestedProxyResource;
+import com.azure.arm.models.resources.models.NestedProxyResourceProperties;
+import com.azure.arm.models.resources.models.NestedProxyResourceUpdateProperties;
+import com.azure.arm.models.resources.models.TopLevelTrackedResource;
+import com.azure.arm.models.resources.models.TopLevelTrackedResourceProperties;
+import com.azure.arm.models.resources.models.TopLevelTrackedResourceUpdateProperties;
 import com.azure.core.http.HttpPipelineBuilder;
 import com.azure.core.management.AzureEnvironment;
 import com.azure.core.management.Region;
 import com.azure.core.management.profile.AzureProfile;
-import com.cadl.server.HttpbinClientBuilder;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import reactor.core.publisher.Mono;
 
-import java.time.OffsetDateTime;
 import java.util.HashMap;
-import java.util.UUID;
 
 public class ArmResourcesTests {
     @Test
@@ -36,18 +34,38 @@ public class ArmResourcesTests {
 //                                },
                         .authenticate(
                                 new HttpPipelineBuilder().build(),
-
-                                new AzureProfile("", UUID.randomUUID().toString(), AZURE));
-        String resourceGroup = "rg-1";
-        String resourceName = "resourceName";
-        TopLevelArmResource topLevelArmResource = resourcesManager.topLevelArmResources()
+                                new AzureProfile("", "00000000-0000-0000-0000-000000000000", AZURE));
+        String resourceGroup = "test-rg";
+        String resourceName = "top";
+        TopLevelTrackedResource topLevelArmResource = resourcesManager.topLevelTrackedResources()
                 .define(resourceName)
                 .withRegion(Region.US_EAST)
                 .withExistingResourceGroup(resourceGroup)
-                .withProperties(new TopLevelArmResourceProperties().withDescription("myDesc"))
+                .withProperties(new TopLevelTrackedResourceProperties().withDescription("valid"))
                 .create();
         Assertions.assertNotNull(topLevelArmResource);
-        topLevelArmResource = resourcesManager.topLevelArmResources().getById(topLevelArmResource.id());
+        topLevelArmResource = resourcesManager.topLevelTrackedResources().getById(topLevelArmResource.id());
         Assertions.assertNotNull(topLevelArmResource);
+
+        Assertions.assertTrue(resourcesManager.topLevelTrackedResources().listByResourceGroup(resourceGroup).stream().findAny().isPresent());
+        Assertions.assertTrue(resourcesManager.topLevelTrackedResources().list().stream().findAny().isPresent());
+
+        topLevelArmResource.update()
+            .withProperties(new TopLevelTrackedResourceUpdateProperties().withDescription("valid2"))
+            .apply();
+
+        resourcesManager.topLevelTrackedResources().deleteById(topLevelArmResource.id());
+
+        NestedProxyResource nestedProxyResource = resourcesManager.nestedProxyResources().define("nested")
+            .withExistingTopLevelTrackedResource(resourceGroup, resourceName)
+            .withProperties(new NestedProxyResourceProperties().withDescription("valid"))
+            .create();
+
+        resourcesManager.nestedProxyResources().getById(nestedProxyResource.id());
+        nestedProxyResource.update()
+            .withProperties(new NestedProxyResourceUpdateProperties().withDescription("valid2"))
+            .apply();
+
+        resourcesManager.nestedProxyResources().deleteById(nestedProxyResource.id());
     }
 }
